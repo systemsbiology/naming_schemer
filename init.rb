615ae24d6406f771e4da1be5e01769aaf3ca1388
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-07-09 11:22:44 -0700

    Reload plugin on each request for development to prevent association errors

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
-# Include hook code here
+# Reload models and controllers on every request in development, to prevent association issues
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/models')
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
